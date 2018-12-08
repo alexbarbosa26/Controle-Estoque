c9643fa9d95a502ed99f8dee371e29179ee91410
Alex Barbosa <abarbosasilva7@gmail.com>
2018-12-07 22:15:02 -0300

    Implementado ProdutoDTO na classe ProdutoResource
    ara que seja validado os dados informados pelo usuario e se estiver todos dentro do conforme salvar o registro no banco de dados.

--- src/main/java/com/InventoryControl/resources/ProdutoResource.java
+++ src/main/java/com/InventoryControl/resources/ProdutoResource.java
@@ -3,6 +3,8 @@ package com.InventoryControl.resources;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.InventoryControl.domain.Produto;
+import com.InventoryControl.dto.ProdutoDTO;
 import com.InventoryControl.services.ProdutoService;
 
 @RestController
@@ -29,7 +32,9 @@ public class ProdutoResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Produto obj) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody ProdutoDTO objDTO) {
+		Produto obj = service.fromDTO(objDTO);
+		
 		obj = service.insert(obj);
 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getCodigo())
--- src/main/java/com/InventoryControl/services/ProdutoService.java
+++ src/main/java/com/InventoryControl/services/ProdutoService.java
@@ -8,49 +8,57 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.InventoryControl.domain.Produto;
+import com.InventoryControl.dto.ProdutoDTO;
 import com.InventoryControl.exceptions.DataIntegrityException;
 import com.InventoryControl.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoService {
-	
+
 	@Autowired
 	private ProdutoRepository repo;
+
+	public Produto insert(Produto obj) {
+
+		return repo.save(obj);
+
+	}
 	
-	public Produto buscarId(Integer cod) {
+	public Produto fromDTO(ProdutoDTO dto) {
 		
+		Produto produto = new Produto(null, dto.getNome(), dto.getQuantidade());
+		
+		return produto;
+		
+	}
+
+	public Produto buscarId(Integer cod) {
+
 		Produto obj = repo.findOne(cod);
-		if(obj == null) {
-			throw new ObjectNotFoundException("Codigo: "+ cod, "não encontrado, favor verificar o codigo digitado!");
+		if (obj == null) {
+			throw new ObjectNotFoundException("Codigo: " + cod, "não encontrado, favor verificar o codigo digitado!");
 		}
 		return obj;
 	}
 
-	public List<Produto> findAll(){
+	public List<Produto> findAll() {
 		return repo.findAll();
 	}
-	
-	public Produto insert(Produto obj) {
-					
-		return repo.save(obj);		
-		
-	}
-	
-	
+
 	public Produto update(Produto obj) {
 		buscarId(obj.getCodigo());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer cod) {
 		buscarId(cod);
 		try {
 			repo.delete(cod);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir o produto");
-			
+
 		}
-		
+
 	}
 
 }
