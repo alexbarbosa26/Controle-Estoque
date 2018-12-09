ee5c5a88ab745d2929866171efc64b4a23589468
Alex Barbosa <abarbosasilva7@gmail.com>
2018-12-09 00:18:12 -0300

    Implementado Insert Sites

--- src/main/java/com/InventoryControl/resources/SiteResource.java
+++ src/main/java/com/InventoryControl/resources/SiteResource.java
@@ -1,12 +1,15 @@
 package com.InventoryControl.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.InventoryControl.domain.Sites;
 import com.InventoryControl.services.SiteService;
@@ -25,5 +28,17 @@ public class SiteResource {
 		List<Sites> objList=service.findAll();
 		
 		return ResponseEntity.ok().body(objList);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Sites obj){
+		
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getCodigo())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
