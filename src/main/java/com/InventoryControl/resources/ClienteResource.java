d8e32800f802de9a7a0e007b0850eb1d249f0571
Alex Barbosa da Silva <abarbosasilva7@gmail.com.br>
2019-02-21 19:12:53 -0300

    Criado a Service de clientes

---
+++ src/main/java/com/InventoryControl/resources/ClienteResource.java
@@ -0,0 +1,10 @@
+package com.InventoryControl.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+}
--- src/main/java/com/InventoryControl/services/ClienteService.java
+++ src/main/java/com/InventoryControl/services/ClienteService.java
@@ -1,8 +1,27 @@
 package com.InventoryControl.services;
 
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.InventoryControl.domain.Cliente;
+import com.InventoryControl.repositories.ClienteRepository;
 
 @Service
 public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repoCliente;
+	
+	//metodo para buscar cliente e suas celulas pelo codigo do cliente
+	public Cliente bucarId(Integer codigo) {
+		
+		Cliente obj = repoCliente.findOne(codigo);
+		
+		if(obj==null) {
+			throw new ObjectNotFoundException("Codigo: "+ codigo, "não encontrado, favor verificar o codigo digitado!");
+		}
+		return obj;
+	}
 
 }
