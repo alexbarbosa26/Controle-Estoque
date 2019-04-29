f3715c7df6012d8fc838c8209b399b5776a2ed16
Alex Barbosa <abarbosasilva7@gmail.com>
2019-04-29 10:05:18 -0300

    Alteração para gerar build para WAR

--- src/main/java/com/InventoryControl/InventoryControlApplication.java
+++ src/main/java/com/InventoryControl/InventoryControlApplication.java
@@ -1,19 +1,20 @@
 package com.InventoryControl;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InventoryControlApplication implements CommandLineRunner {
-
+public class InventoryControlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryControlApplication.class, args);
 	}
-
+	
 	@Override
-	public void run(String... args) throws Exception {
-
-			}
+	protected SpringApplicationBuilder configure(
+	        SpringApplicationBuilder application) {
+	    return application.sources(InventoryControlApplication.class);
+	}
 }
--- src/main/java/com/InventoryControl/config/TestConfig.java
+++ src/main/java/com/InventoryControl/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.InventoryControl.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,19 +13,25 @@ import com.InventoryControl.services.EmailService;
 import com.InventoryControl.services.SmtpEmailService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
 	
 	@Autowired
-	private DBService service;
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		
-		service.instantiateTestDataBase();
+		if (!"create".equals(strategy)) {
+			return false;
+		}
 		
+		dbService.instantiateTestDataBase();
 		return true;
-	}
+}
 	/*
 	@Bean
 	public EmailService emailService() {
